fca77ac1be9f21b049d61f0d07477da698ec4cc4
bc-dipesh <bcdipeshwork@gmail.com>
2020-11-26 22:04:03 +0545

    REFRACTOR: Update method calls in CommandTxtBox_KeyDown

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace graphical_programming_language
@@ -13,21 +6,20 @@ namespace graphical_programming_language
     public partial class mainForm : Form
     {
         private ShapeCompiler shapeCompiler;
-        private Shape shape;
         public mainForm()
         {
             InitializeComponent();
-            shapeCompiler = new ShapeCompiler();
+            shapeCompiler = new ShapeCompiler(codeOutputPanel);
         }
 
         private void CommandTxtBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
             {
-                shapeCompiler.Compile(commandTxtBox.Text);
-                shape = shapeCompiler.Run();
+                var command = commandTxtBox.Text;
 
-                shape.Draw(codeOutputPanel.CreateGraphics());
+                shapeCompiler.Compile(command);
+                shapeCompiler.Run();
             }
         }
     }
