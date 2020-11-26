0429986d0a8804be647ca9fdd854537589e2d228
bc-dipesh <bcdipeshwork@gmail.com>
2020-11-26 22:13:58 +0545

    FEAT: Add clear, exit command. Update run method to draw shapes in the panel

--- ShapeCompiler.cs
+++ ShapeCompiler.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace graphical_programming_language
 {
     internal class ShapeCompiler : IShapeCompiler
     {
         private ShapeFactory shapeFactory;
+        private Panel drawingPanel;
         private string command;
         private string[] arguments;
         private Regex splitOnSpaces;
@@ -14,32 +15,47 @@ namespace graphical_programming_language
         public ShapeCompiler()
         {
             shapeFactory = new ShapeFactory();
-            arguments = new string[3];
             splitOnSpaces = new Regex(@"\s+", RegexOptions.Compiled);
         }
 
-        public List<string> Compile(string code)
+        public ShapeCompiler(Panel panel)
+        {
+            shapeFactory = new ShapeFactory();
+            splitOnSpaces = new Regex(@"\s+", RegexOptions.Compiled);
+            drawingPanel = panel;
+        }
+
+        public void Compile(string code)
         {
             string[] data = splitOnSpaces.Split(code);
 
             command = data[0];
-            Array.Copy(data, 1, arguments, 0, data.Length - 1);
+            arguments = new string[data.Length - 1];
 
-            return new List<string>();
+            if (data.Length > 1)
+            {
+                Array.Copy(data, 1, arguments, 0, arguments.Length);
+            }
         }
 
-        public Shape Run()
+        public void Run()
         {
-            Shape shape = null;
             if (command.ToUpper().Equals("DRAW"))
             {
-                if (arguments.Length > 1)
+                if (arguments.Length == 1)
                 {
-                    shape = shapeFactory.getShape(arguments[0]);
+                    Shape shape = shapeFactory.getShape(arguments[0]);
+                    shape.Draw(drawingPanel.CreateGraphics());
                 }
             }
-
-            return shape;
+            else if (command.ToUpper().Equals("CLEAR"))
+            {
+                drawingPanel.Refresh();
+            }
+            else if (command.ToUpper().Equals("EXIT"))
+            {
+                Application.Exit();
+            }
         }
     }
 }
