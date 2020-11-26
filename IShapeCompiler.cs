f5242aec7ce9a1fe0d0f22483351cfb30b39aeaa
bc-dipesh <bcdipeshwork@gmail.com>
2020-11-26 22:01:25 +0545

    REFRACTOR: Change return type of Compile and Run to void

--- IShapeCompiler.cs
+++ IShapeCompiler.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace graphical_programming_language
+﻿namespace graphical_programming_language
 {
     internal interface IShapeCompiler
     {
-        List<string> Compile(string code);
+        void Compile(string code);
 
-        Shape Run();
-
+        void Run();
     }
 }
