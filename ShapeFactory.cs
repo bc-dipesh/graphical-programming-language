65556fb4a48de5666d35fdd3e304df53fa032a85
bc-dipesh <bcdipeshwork@gmail.com>
2020-11-27 08:32:56 +0545

    FEAT: Add ability to create shape if user give draw position and size of shape

--- ShapeFactory.cs
+++ ShapeFactory.cs
@@ -1,14 +1,31 @@
-﻿namespace graphical_programming_language
+﻿using System.Drawing;
+
+namespace graphical_programming_language
 {
     internal class ShapeFactory
     {
-        public Shape getShape(string shapeType)
+        public Shape GetShape(string shapeType)
         {
             shapeType = shapeType.ToUpper().Trim();
 
             if (shapeType.Equals("RECTANGLE"))
             {
                 return new Rectangle();
+            }
+            else
+            {
+                System.ArgumentException argumentException = new System.ArgumentException($"Factory error: {shapeType} does not exist");
+                throw argumentException;
+            }
+        }
+
+        public Shape GetShape(string shapeType, Color color, int x, int y, int width, int height)
+        {
+            shapeType = shapeType.ToUpper().Trim();
+
+            if (shapeType.Equals("RECTANGLE"))
+            {
+                return new Rectangle(color, x, y, width, height);
             }
             else
             {
