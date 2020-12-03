da9b26617f7c908ab2836d9ab69ff91ad1378ebc
bc-dipesh <bcdipeshwork@gmail.com>
2020-12-03 20:03:48 +0545

    FEAT: Display about message on about menut item click

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Windows.Forms;
 
 namespace graphical_programming_language
@@ -6,11 +7,32 @@ namespace graphical_programming_language
     public partial class mainForm : Form
     {
         private readonly ShapeCompiler shapeCompiler;
+        private StringBuilder aboutMessage;
 
         public mainForm()
         {
             InitializeComponent();
+            SetUpAboutMessage();
+
             shapeCompiler = new ShapeCompiler(outputWindow, programWindow, programLog);
+        }
+
+        private void SetUpAboutMessage()
+        {
+            aboutMessage = new StringBuilder();
+            aboutMessage.Append("A custom programming language made using C# that");
+            aboutMessage.Append(Environment.NewLine);
+            aboutMessage.Append("demonstrates the basics of sequence, selection and iteration");
+            aboutMessage.Append(Environment.NewLine);
+            aboutMessage.Append("and allows a student programmer to explore them using graphics.");
+            aboutMessage.Append(Environment.NewLine);
+            aboutMessage.Append(Environment.NewLine);
+            aboutMessage.Append(Environment.NewLine);
+            aboutMessage.Append("Author: Dipesh BC");
+            aboutMessage.Append(Environment.NewLine);
+            aboutMessage.Append("Version: 1.0");
+            aboutMessage.Append(Environment.NewLine);
+            aboutMessage.Append("License: MIT");
         }
 
         private void CommandTxtBox_KeyDown(object sender, KeyEventArgs e)
@@ -70,7 +92,12 @@ namespace graphical_programming_language
 
         private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-           Application.Exit();
+            Application.Exit();
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(aboutMessage.ToString(), "About", MessageBoxButtons.OK, MessageBoxIcon.Question);
         }
     }
 }
